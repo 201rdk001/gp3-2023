b9f5b9e3d14dccec821436b9c0fa572c7cb7ffe4
Kristaps Arnolds Kaidalovs <Kristaps-Arnolds.Kaidalovs@edu.rtu.lv>
2023-04-15 22:20:12 +0300

    Added Main class

---
+++ Main.java
@@ -0,0 +1,28 @@
+// 201RDB165 Mārtiņš Daugavietis 15.grupa
+// 201RDK001 Kristaps Arnolds Kaidalovs 16.grupa
+// 201RDB202 Keita Laimiņa 16.grupa
+
+class Zstd {
+    public void compress() {
+    }
+
+    public void decompress() {
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+    }
+
+    public static void compress() {
+    }
+
+    public static void decompress() {
+    }
+
+    public static void equal() {
+    }
+
+    public static void size() {
+    }
+}
