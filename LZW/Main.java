5bd2543cffe6a1fe2a02cc13f47a52e43695b78d
Kristaps Arnolds Kaidalovs <Kristaps-Arnolds.Kaidalovs@edu.rtu.lv>
2023-04-29 21:05:09 +0300

    General "fixes"

--- LZW/Main.java
+++ LZW/Main.java
@@ -3,297 +3,275 @@
 // 221RDB023 Keita Laimiņa 16.grupa
 
 import java.io.File;
-import java.io.FileReader;
-import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.util.Scanner;
-import java.util.List;
 import java.util.ArrayList;
-import java.util.Map;
 import java.util.HashMap;
 
-class CodeInputStream extends InputStream {
-    private final InputStream in;
-    private int codeBuf;
-    private int bitsFree;
-    private boolean eof;
-    
-    public int codeLength;
-    
-    public CodeInputStream(InputStream stream) {
-        in = stream;
-        codeLength = 9;
-        bitsFree = Integer.SIZE;
-        eof = false;
+class BitStreamInput {
+    private InputStream stream;
+    private int buf;
+    private int usedBits;
+    public int bitLen;
+
+    public BitStreamInput(InputStream stream) {
+        this.stream = stream;
+        this.bitLen = 9;
+        this.usedBits = Integer.SIZE;
     }
 
-    @Override
     public int read() throws IOException {
-        int num = -1;
-        
+        int byteIn;
+
         do {
-            num = in.read();
-        
-            if (num == -1)
+            if ((byteIn = stream.read()) == -1)
                 break;
-                
-            codeBuf = codeBuf | num << (bitsFree - 8);
-            bitsFree -= 8;	
-        } while (num != -1 && bitsFree >= 8);
-            
-        int value = codeBuf >>> (Integer.SIZE - codeLength);
-        codeBuf = codeBuf << codeLength;
-        bitsFree += codeLength;
-        
-        if (num == -1)
-            eof = bitsFree >= Integer.SIZE - codeLength;
-        
+
+            buf = buf | byteIn << (usedBits - 8);
+            usedBits -= 8;
+        } while (byteIn != -1 && usedBits >= 8);
+
+        int value = buf >>> (Integer.SIZE - bitLen);
+        buf = buf << bitLen;
+        usedBits += bitLen;
+
         return value;
     }
-    
+
     public boolean eof() {
-        return eof;
+        return usedBits >= Integer.SIZE - bitLen;
     }
 }
 
-class CodeOutputStream extends OutputStream {
-    private final OutputStream out;
-    private int codeBuf;
-    private int bitsFree;
-    
-    public int codeLength;
-    
-    public CodeOutputStream(OutputStream stream) {
-        this.out = stream;
-        this.codeLength = 9;
-        this.bitsFree = Integer.SIZE;
+class BitStreamOutput {
+    private OutputStream stream;
+    private int buf;
+    private int usedBits;
+    public int bitLen;
+
+    public BitStreamOutput(OutputStream stream) {
+        this.stream = stream;
+        this.bitLen = 9;
+        this.usedBits = Integer.SIZE;
     }
-    
-    @Override
+
     public void write(int value) throws IOException {
-        codeBuf = codeBuf | value << (bitsFree - codeLength);
-        bitsFree -= codeLength;
-        
-        while (bitsFree <= Integer.SIZE - 8) {
-            out.write(codeBuf >>> 24);
-            codeBuf = codeBuf << 8;
-            bitsFree += 8;
+        buf = buf | value << (usedBits - bitLen);
+        usedBits -= bitLen;
+
+        while (usedBits <= Integer.SIZE - 8) {
+            stream.write(buf >>> 24);
+            buf = buf << 8;
+            usedBits += 8;
         }
     }
-    
+
     public void flush() throws IOException {
-        while (bitsFree < Integer.SIZE && codeBuf != 0) {
-            out.write(codeBuf >>> 24);
-            codeBuf = codeBuf << 8;
-            bitsFree += 8;
+        while (usedBits < Integer.SIZE && buf != 0) {
+            stream.write(buf >>> 24);
+            buf = buf << 8;
+            usedBits += 8;
         }
     }
 }
 
 class LZW {
-    public static void compress(InputStream in, OutputStream out) throws IOException {
-        Map<String, Integer> dictionary = new HashMap<>();
-        CodeOutputStream bout = new CodeOutputStream(out);
-        
-        for (int i = 0; i < 256; i++)
-            dictionary.put(Character.toString((char)i), i);
-        
-        String str = "";
+    public static void compress(FileInputStream in, BitStreamOutput binaryOut) throws IOException {
+        HashMap<String, Integer> dict = new HashMap<String, Integer>();
+
+        for (int i = 0; i < (1 << Byte.SIZE); i++)
+            dict.put(Character.toString((char)i), i);
+
+        String currStr = "";
         int byteIn;
 
         while ((byteIn = in.read()) != -1) {
             char sym = (char)byteIn;
-            String strSym = str + sym;
-            
-            if (dictionary.containsKey(strSym)) {
-                str = strSym;
-            }
-            else {
-                bout.write(dictionary.get(str));
-                dictionary.put(strSym, dictionary.size());
-                str = Character.toString(sym);
-            }
-            
-            if (dictionary.size() >= 1 << bout.codeLength)
-                bout.codeLength++;
-        }
-        
-        if (!str.equals(""))
-            bout.write(dictionary.get(str));
-        
-        bout.flush();
-    }
-    
-    public static void decompress(InputStream in, OutputStream out) throws IOException {
-        List<String> dictionary = new ArrayList<String>(256);
-        CodeInputStream bin = new CodeInputStream(in);
-        DataOutputStream dout = new DataOutputStream(out);
-        
-        for (int i = 0; i < 256; i++)
-            dictionary.add(Character.toString((char)i));
+            String currStrSymbol = currStr + sym;
 
-        String entry;
-        char ch;
-        int prevCode, currCode;
-        
-        prevCode = bin.read();
-        entry = dictionary.get(prevCode);
-        dout.writeBytes(entry);
-        ch = entry.charAt(0);
-        
-        while (!bin.eof()) {
-            currCode = bin.read();
-            
-            if (currCode >= dictionary.size()) {
-                entry = dictionary.get(prevCode);
-                entry += ch;
+            if (dict.containsKey(currStrSymbol)) {
+                currStr = currStrSymbol;
             }
             else {
-                entry = dictionary.get(currCode);
+                binaryOut.write(dict.get(currStr));
+                dict.put(currStrSymbol, dict.size());
+                currStr = Character.toString(sym);
             }
-            
-            dout.writeBytes(entry);
-            ch = entry.charAt(0);
-            dictionary.add(dictionary.get(prevCode)+ch);
-            prevCode = currCode;
-            
-            if (dictionary.size() + 1 >= 1 << bin.codeLength)
-                bin.codeLength++;
+
+            if (dict.size() >= 1 << binaryOut.bitLen)
+                binaryOut.bitLen++;
+        }
+
+        if (!currStr.equals(""))
+            binaryOut.write(dict.get(currStr));
+
+        binaryOut.flush();
+    }
+
+    public static void decompress(BitStreamInput binaryIn, DataOutputStream dataOut) throws IOException {
+        ArrayList<String> dict = new ArrayList<String>((1 << Byte.SIZE));
+
+        for (int i = 0; i < (1 << Byte.SIZE); i++)
+            dict.add(Character.toString((char)i));
+
+        String str;
+        char ch;
+        int prevIdx, currIdx;
+
+        prevIdx = binaryIn.read();
+        str = dict.get(prevIdx);
+        dataOut.writeBytes(str);
+        ch = str.charAt(0);
+
+        while (!binaryIn.eof()) {
+            currIdx = binaryIn.read();
+
+            if (currIdx >= dict.size()) {
+                str = dict.get(prevIdx);
+                str += ch;
+            }
+            else {
+                str = dict.get(currIdx);
+            }
+
+            dataOut.writeBytes(str);
+            ch = str.charAt(0);
+            dict.add(dict.get(prevIdx)+ch);
+            prevIdx = currIdx;
+
+            if (dict.size() + 1 >= 1 << binaryIn.bitLen)
+                binaryIn.bitLen++;
         }
     }
 }
 
 public class Main {
-    public static Scanner sc = new Scanner(System.in);
-    
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) throws IOException {
-        int izv = 0;
-        do {
-            System.out.println("\n1. compress");
-            System.out.println("2. decompress");
-            System.out.println("3. size");
-            System.out.println("4. equal");
-            System.out.println("5. about");
-            System.out.println("6. exit\n");
-            izv = sc.nextInt();
-            switch(izv) {
-            case 1:
-                compress();
-                break;
-            case 2:
-                decompress();
-                break;
-            case 3:
-                size();
-                break;
-            case 4:
-                equal();
-                break;
-            case 5:
-                System.out.println("221RDB019 Mārtiņš Daugavietis 15.grupa");
-                System.out.println("201RDK001 Kristaps Arnolds Kaidalovs 16.grupa");
-                System.out.println("221RDB023 Keita Laimiņa 16.grupa");
-                break;
-            case 6:
-                System.out.println("Compressor closed");
-                break;
-            default:
-                System.out.println("Error, wrong number");
-            break;
-          }
-        } while (izv != 6);
-        sc.close();
-        
-    }
-    
-    public static void compress() {
-        System.out.println("Source file name:");
-        sc.nextLine();
-        String inFilename=sc.nextLine();
-        System.out.println("Archive name:");  
-        String outFilename=sc.nextLine();  
-        try {
-            InputStream in;
-            OutputStream out;
+        String command;
 
-            in = new BufferedInputStream(new FileInputStream(inFilename));
-            out = new BufferedOutputStream(new FileOutputStream(outFilename));
-            LZW.compress(in, out);
+        do {
+            System.out.print("command>");
+            command = sc.nextLine();
+            switch (command) {
+                case "comp": compress(); break;
+                case "decomp": decompress(); break;
+                case "equal": equal(); break;
+                case "size": size(); break;
+                case "about": about(); break;
+                case "exit": break;
+                default: System.out.println("unknown command");
+          }
+        } while (!command.equals("exit"));
+
+        sc.close();
+    }
+
+    public static void compress() {
+        System.out.println("source file name:");
+        String source=sc.nextLine();
+        System.out.println("archive name:");
+        String archive=sc.nextLine();
+
+        try {
+            FileInputStream in = new FileInputStream(source);
+            FileOutputStream out = new FileOutputStream(archive);
+            BitStreamOutput bout = new BitStreamOutput(out);
+
+            LZW.compress(in, bout);
+
             in.close();
             out.close();
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (Exception ex) {
+            System.out.println("Error opening or reading file, check input");
+            System.out.println(ex.getMessage());
         }
     }
 
     public static void decompress() {
-        System.out.println("Archive name:");
-        sc.nextLine();
-        String inFilename=sc.nextLine();
-        System.out.println("Source file name:");  
-        String outFilename=sc.nextLine();  
-        try {
-            InputStream in;
-            OutputStream out;
+        System.out.println("archive name:");
+        String archive=sc.nextLine();
+        System.out.println("file name:");
+        String source=sc.nextLine();
 
-            in = new BufferedInputStream(new FileInputStream(inFilename));
-            out = new BufferedOutputStream(new FileOutputStream(outFilename));
-            LZW.decompress(in, out);
+        try {
+            FileInputStream in = new FileInputStream(archive);
+            BitStreamInput bin = new BitStreamInput(in);
+            FileOutputStream out = new FileOutputStream(source);
+            DataOutputStream dout = new DataOutputStream(out);
+
+            LZW.decompress(bin, dout);
+
             in.close();
             out.close();
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (Exception ex) {
+            System.out.println("Error opening or reading file, check input");
+            System.out.println(ex.getMessage());
         }
     }
 
     public static void equal() throws IOException {
-        System.out.println("First file name:");
-        sc.nextLine();
-        String inFilename1=sc.nextLine();
-        System.out.println("Second file name:");
-        String inFilename2=sc.nextLine();
+        System.out.print("first file name: ");
+        String file1 = sc.nextLine();
+        System.out.print("second file name: ");
+        String file2 = sc.nextLine();
         try {
-            BufferedReader r1 = new BufferedReader(new FileReader(inFilename1));
-            BufferedReader r2 = new BufferedReader(new FileReader(inFilename2));
-            String s1 = r1.readLine();
-            String s2 = r2.readLine();
-            boolean same = true;
-            while (s1 != null || s2 != null){
-                if(s1 == null || s2 == null){
-                    same = false;
-                    break;
-                }else if(! s1.equals(s2)) {
-                    same = false; 
-                       break;
+            FileInputStream f1 = new FileInputStream(file1);
+            FileInputStream f2 = new FileInputStream(file2);
+            int size1, size2;
+            byte[] buf1 = new byte[1000];
+            byte[] buf2 = new byte[1000];
+            do {
+                size1 = f1.read(buf1);
+                size2 = f2.read(buf2);
+                if (size1 != size2) {
+                    f1.close();
+                    f2.close();
+                    System.out.println(false);
+                    return;
                 }
-                s1 = r1.readLine();
-                s2 = r2.readLine();
-            } 
-            r1.close();
-            r2.close();
-            System.out.println(same);
-        } catch (Exception e) {
-            e.printStackTrace();
+
+                for (int i=0; i < size1; i++) {
+                    if (buf1[i] != buf2[i]) {
+                        f1.close();
+                        f2.close();
+                        System.out.println(false);
+                        return;
+                }
+            }
+            } while (size1 == 0 && size2 == 0);
+
+            f1.close();
+            f2.close();
+            System.out.println(true);
+            return;
+        } catch (IOException ex) {
+            System.out.println("Error opening or reading file, check input");
+            System.out.println(ex.getMessage());
+            System.out.println(false);
         }
     }
-    
+
     public static void size() {
-        System.out.println("File name:");
-        sc.nextLine();
-        String Filename=sc.nextLine();
+        System.out.println("file name");
+        String Filename = sc.nextLine();
         try {
             File file = new File(Filename);
-            long bytes = file.length();
-            System.out.println("Size: " + bytes);
-        } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println("size: " + file.length());
+        } catch (Exception ex) {
+            System.out.println("Error opening or reading file, check input");
+            System.out.println(ex.getMessage());
         }
+    }
+
+    public static void about() {
+        System.out.println("221RDB019 Mārtiņš Daugavietis 15.grupa");
+        System.out.println("201RDK001 Kristaps Arnolds Kaidalovs 16.grupa");
+        System.out.println("221RDB023 Keita Laimiņa 16.grupa");
     }
 }
